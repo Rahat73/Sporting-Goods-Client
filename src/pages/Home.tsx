35509e47048bd54fec34696dbab7b8f2c90f10e9
Rahat73 <rahat.ashik.18@gmail.com>
2024-07-13 22:09:54 +0600

    added framer motion

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import CategorySection from "../components/home/CategorySection";
 import ContactUsSection from "../components/home/ContactUsSection";
 import FeaturedSection from "../components/home/FeaturedSection";
 import HeroSection from "../components/home/HeroSection";
+import { motion } from "framer-motion";
 
 const Home = () => {
   useEffect(() => {
@@ -12,9 +13,36 @@ const Home = () => {
   return (
     <div>
       <HeroSection />
-      <FeaturedSection />
-      <CategorySection />
-      <ContactUsSection />
+      <div className="my-10">
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.75, delay: 0.15 }}
+          viewport={{ once: true, amount: 0.5 }}
+        >
+          <FeaturedSection />
+        </motion.div>
+      </div>
+      <div className="my-10">
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.75, delay: 0.15 }}
+        >
+          <CategorySection />
+        </motion.div>
+      </div>
+      <div>
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.75, delay: 0.15 }}
+          viewport={{ once: true, amount: 0.5 }}
+        >
+          <ContactUsSection />
+        </motion.div>
+      </div>
     </div>
   );
 };
